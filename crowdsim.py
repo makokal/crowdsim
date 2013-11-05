9b8995f4370e3c525e46d739b19975fd0406bd38
Billy Okal <okal.billy@gmail.com>
2013-11-05 18:30:17 +0100

    minor tests

--- crowdsim.py
+++ crowdsim.py
@@ -203,7 +203,7 @@ class Simulation(object):
         # add some obstacles
         self.obstacles = []
         # self.obstacles.append(Obstacle(self.screen, 'box', 'Rect', (2.5,3,1,1)))
-        # self.obstacles.append(Obstacle(self.screen, 'line', 'Line', (4,3,3,4)))
+        self.obstacles.append(Obstacle(self.screen, 'line', 'Line', (4,3,3,4)))
         self.obstacles.append(Obstacle(self.screen, 'tree', 'Circle', (3.5,2.5,0.4,0)))
         self.obstacles.append(Obstacle(self.screen, 'tree', 'Circle', (2.5,3.5,0.4,0)))
 
