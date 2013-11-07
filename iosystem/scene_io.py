8577ad229576fe46bc9f188b2fcd8d41f87b15bd
Billy Okal <okal.billy@googlemail.com>
2013-11-07 01:30:57 +0100

    moving minor changes about

--- iosystem/scene_io.py
+++ iosystem/scene_io.py
@@ -4,75 +4,8 @@ try:
 except ImportError:
     import xml.etree.ElementTree as ET
 
-
-def xmltodict(element):
-    if not isinstance(element, ET.Element):
-        raise ValueError("must pass xml.etree.ElementTree.Element object")
-
-    def xmltodict_handler(parent_element):
-        result = dict()
-        for element in parent_element:
-            if len(element):
-                obj = xmltodict_handler(element)
-            else:
-                obj = element.text
-
-            if result.get(element.tag):
-                if hasattr(result[element.tag], "append"):
-                    result[element.tag].append(obj)
-                else:
-                    result[element.tag] = [result[element.tag], obj]
-            else:
-                result[element.tag] = obj
-        return result
-
-    return {element.tag: xmltodict_handler(element)}
-
-
-def dicttoxml(element):
-    if not isinstance(element, dict):
-        raise ValueError("must pass dict type")
-    if len(element) != 1:
-        raise ValueError("dict must have exactly one root key")
-
-    def dicttoxml_handler(result, key, value):
-        if isinstance(value, list):
-            for e in value:
-                dicttoxml_handler(result, key, e)
-        elif isinstance(value, basestring):
-            elem = ET.Element(key)
-            elem.text = value
-            result.append(elem)
-        elif isinstance(value, int) or isinstance(value, float):
-            elem = ET.Element(key)
-            elem.text = str(value)
-            result.append(elem)
-        elif value is None:
-            result.append(ET.Element(key))
-        else:
-            res = ET.Element(key)
-            for k, v in value.items():
-                dicttoxml_handler(res, k, v)
-            result.append(res)
-
-    result = ET.Element(element.keys()[0])
-    for key, value in element[element.keys()[0]].items():
-        dicttoxml_handler(result, key, value)
-    return result
-
-def xmlfiletodict(filename):
-    return xmltodict(ET.parse(filename).getroot())
-
-def dicttoxmlfile(element, filename):
-    ET.ElementTree(dicttoxml(element)).write(filename)
-
-def xmlstringtodict(xmlstring):
-    return xmltodict(ET.fromstring(xmlstring).getroot())
-
-def dicttoxmlstring(element):
-    return ET.tostring(dicttoxml(element))
-
-
+from collections import defaultdict
+from pprint import pprint
 
 
 
@@ -86,12 +19,13 @@ class SceneIO(object):
 
         """
         self._scene_file = scene_file
-        self._dict = xmlfiletodict(self._scene_file)
+        self._tree = ET.parse(self._scene_file).getroot()
+        self._dict = self._etree_to_dict(self._tree)
 
 
     def load_scene(self, scene_file):
-        self._scene_file = scene_file
-        self._dict = xmlfiletodict(self._scene_file)
+        self._tree = ET.parse(self._scene_file).getroot()
+        self._dict = self._etree_to_dict(self._tree)
 
         # do the xml magic
         pass
@@ -99,9 +33,30 @@ class SceneIO(object):
 
     def save_scene(self, filename):
         # TODO - Check filename validity
-        xml_scene = dicttoxml(self._dict)
-        tree = ET.ElementTree(xml_scene.getroot())
+        tree = ET.ElementTree(self._tree)
         tree.write(filename)
+
+    def _etree_to_dict(self, t):
+        d = {t.tag: {} if t.attrib else None}
+        children = list(t)
+        if children:
+            dd = defaultdict(list)
+            for dc in map(self._etree_to_dict, children):
+                for k, v in dc.iteritems():
+                    dd[k].append(v)
+            d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
+        if t.attrib:
+            d[t.tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
+        if t.text:
+            text = t.text.strip()
+            if children or t.attrib:
+                if text:
+                  d[t.tag]['#text'] = text
+            else:
+                d[t.tag] = text
+        return d
+
+
 
 
 
--- run_simulation.py
+++ run_simulation.py
@@ -2,31 +2,7 @@
 from crowdsim import Simulation
 from iosystem import SceneIO
 
-
-import xml.etree.ElementTree as ET
-from collections import defaultdict
 from pprint import pprint
-
-
-def etree_to_dict(t):
-    d = {t.tag: {} if t.attrib else None}
-    children = list(t)
-    if children:
-        dd = defaultdict(list)
-        for dc in map(etree_to_dict, children):
-            for k, v in dc.iteritems():
-                dd[k].append(v)
-        d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.iteritems()}}
-    if t.attrib:
-        d[t.tag].update(('@' + k, v) for k, v in t.attrib.iteritems())
-    if t.text:
-        text = t.text.strip()
-        if children or t.attrib:
-            if text:
-              d[t.tag]['#text'] = text
-        else:
-            d[t.tag] = text
-    return d
 
 
 def start_main_simulation(params):
@@ -42,13 +18,13 @@ if __name__ == '__main__':
             }
 
 
-    # sio = SceneIO('scenes/simple_room.xml')
+    sio = SceneIO('scenes/simple_room.xml')
     # print sio._dict
 
-    tree = ET.parse('scenes/simple_room.xml')
-    d = etree_to_dict(tree.getroot())
-    pprint(d['simulation']['agent'])
-    pprint(d['simulation']['obstacle'])
-    pprint(d['simulation']['waypoint'])
+    # tree = ET.parse('scenes/simple_room.xml')
+    # d = etree_to_dict(tree.getroot())
+    pprint(sio._dict['simulation']['agent'])
+    pprint(sio._dict['simulation']['obstacle'])
+    pprint(sio._dict['simulation']['waypoint'])
 
     # start_main_simulation(params)
