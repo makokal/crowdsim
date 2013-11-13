2f38a440567b3edd5030f6f6d6656ba49df8d1c0
Billy Okal <okal.billy@googlemail.com>
2013-11-13 22:32:12 +0100

    started working on the new ui using kivy instead of pygame

---
+++ ui/widgets.py
@@ -0,0 +1,23 @@
+import kivy
+
+from kivy.uix.widget import Widget
+
+
+class AgentView(Widget):
+    """ The view for agents """
+    pass 
+
+
+class WaypointView(Widget):
+    """ The view for Waypoints """
+    pass 
+
+
+class ObstacleView(Widget):
+    """ The view for Obstacles """
+    pass    
+
+
+class GridView(Widget):
+    """ The view for Grids """
+    pass    
