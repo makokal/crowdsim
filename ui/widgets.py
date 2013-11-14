76a179206ffbad2f57280e39fab649ebc229a9b5
Billy Okal <okal.billy@gmail.com>
2013-11-14 18:29:12 +0100

    initial pieces of kivy

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,7 +1,8 @@
 import kivy
 
+from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.graphics import Color, Ellipse, Line, Rectangle
 
 class AgentView(Widget):
     """ The view for agents """
@@ -16,6 +17,31 @@ class WaypointView(Widget):
 class ObstacleView(Widget):
     """ The view for Obstacles """
     pass    
+
+
+class Cell(Widget):
+    def __init__(self, px, py):
+        super(Cell, self).__init__()
+        self.px, self.py = px, py
+        with self.canvas:
+            Color(1, 0.2, 0,2)
+            Rectangle(pos=(self.px, self.py), size=(40,40))
+
+
+class Field(Widget):
+    def __init__(self, **kwargs):
+        super(Field, self).__init__(**kwargs)
+        with self.canvas:
+            Color(0.2, 0.2, 0,2)
+            Rectangle(pos=(0,0), size=(600,600))
+
+
+    def on_touch_down(self, touch):
+        with self.canvas:
+            Color(1, 0.2, 0,2)
+            Rectangle(pos=(touch.x, touch.y), size=(40,40))
+
+
 
 
 class GridView(Widget):
